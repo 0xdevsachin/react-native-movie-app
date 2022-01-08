e56619a25635b5d98631e2bf5f9b835952ae1d30
sachin320 <sachinvermavs786@gmail.com>
2022-01-08 23:06:13 +0530

    Project Initialized

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { View, Text } from "react-native";
+
+const App = () =>{
+  return(
+    <View>
+      <Text>Hello World!</Text>
+    </View>
+  )
+}
+
+
+export default App;
