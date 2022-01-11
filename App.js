dd2de23c95d1787eb0ec4fe7c83f39c68e0de856
sachin320 <sachinvermavs786@gmail.com>
2022-01-11 19:28:11 +0530

    Header Added

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
 import React from "react";
 
-import { View, Text } from "react-native";
-
+import { View, Text, StyleSheet } from "react-native";
+import Header from "./src/components/header/header";
 const App = () =>{
   return(
-    <View>
-      <Text>Hello World!</Text>
+    <View style={styles.container}>
+      <Header />
     </View>
   )
 }
 
 
 export default App;
+
+const styles = StyleSheet.create({
+  container : {
+    backgroundColor : '#1b212f',
+    height : 100,
+    flex : 1
+  }
+})
---
+++ src/components/header/header.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import {Text, View, Image, StyleSheet} from 'react-native';
+
+import navIcon from '../../assets/icon.png'
+const Header = () => {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.text}>Explore</Text>
+        <Text style={{color : 'white', marginTop : 5}}>Welcome Back John!</Text>
+      </View>
+      <View >
+        <Image style={styles.img} source={navIcon} />
+      </View>
+    </View>
+  );
+};
+
+export default Header;
+
+
+const styles = StyleSheet.create({
+    container : {
+        padding : 20,
+        flexDirection : 'row',
+        justifyContent : 'space-between',
+        alignItems : 'center'
+    },
+    img : {
+        width : 50,
+        height: 50,
+    },
+    text : {
+        fontSize : 23,
+        color :'white',
+    }
+})
