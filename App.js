164b31d5011a1e9f7f84bd381cb1d121d163ffe2
sachin320 <sachinvermavs786@gmail.com>
2022-01-17 23:34:35 +0530

    Navigation added

--- App.js
+++ App.js
@@ -1,80 +1,38 @@
-import React, {useEffect, useState} from "react";
+import React from 'react';
+import HomeScreen from './src/screen/Home/home';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { Text, View } from 'react-native';
+import Detail from './src/screen/Detail/detail';
+const Stack = createNativeStackNavigator();
 
-import { View, Text, StyleSheet, Image, ScrollView } from "react-native";
-import Header from "./src/components/header/header";
-import Poster from "./src/components/poster/poster";
-import requests from "./src/requests";
-import axios from "axios";
-import Genere from "./src/components/Genere/genere";
-const App = () =>{
-  const [genere, setGenere] = useState([])
-  // const genere = ['Action', 'Comedy', 'Horror', 'Romance', 'Documentaries']
-  const[data, setData] = useState([])
-  useEffect(() =>{
-     const fetchData = async () =>{
-      await axios.get(`${requests.baseUrl}${requests.fetchTrending}`)
-      .then((res) =>{
-        setData(res.data.results)
-      })
-      .catch((err) => console.log(err))
-      await axios.get(`${requests.baseUrl}${requests.genereList}`)
-      .then((res) =>{
-        setGenere(res.data.genres)
-      })
-     }
-     fetchData()
-  }, [])
-  return(
-    <View style={styles.container}>
-      <ScrollView>
-      <Header />
-      <View>
-        <Text style={styles.text}>Trending Now</Text>
-      </View>
-     <View style={styles.poster}>
-       <ScrollView horizontal={true}>
-     {
-       data.map(item =>{
-         return <Poster title={item.title} key={item.id} link={`${requests.imageUrl}${item.poster_path}`} />
-       })
-     }
-     </ScrollView>
-     </View>
-     <View style={styles.genere}>
-       <Text style={styles.text}>Best Genre</Text>
-       <ScrollView horizontal={true}>
-         {
-           genere.map(item => <Genere key={item.name} title={item.name} /> ) 
-         }
-       </ScrollView>
-     </View>
-     
-    </ScrollView>
-    </View>
-  )
-}
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen options={{
+          title: 'Home',
+          headerStyle: {
+            backgroundColor: '#1b212f',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }} name="Home" component={HomeScreen} />
+        <Stack.Screen options={{
+          headerStyle: {
+            backgroundColor: '#1b212f',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }} name="Genere" component={Detail} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 
 export default App;
-
-const styles = StyleSheet.create({
-  container : {
-    backgroundColor : '#1b212f',
-    height : 100,
-    flex : 1
-  },
-  poster: {
-    height : 400,
-    flexDirection :'row',
-    overflow : 'scroll',
-  },
-  text :{
-    color : '#fff',
-    paddingLeft : 20,
-    fontSize : 25,
-    marginTop : 15
-  },
-  genere : {
-    marginBottom : 20
-  }
-})
