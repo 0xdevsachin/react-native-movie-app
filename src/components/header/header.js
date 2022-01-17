8b7978afd9927dd2032730df4a0e6bc0b223de46
sachin320 <sachinvermavs786@gmail.com>
2022-01-18 01:59:09 +0530

    Greet Message Added

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import {Text, View, Image, StyleSheet} from 'react-native';
 
 import navIcon from '../../assets/icon.png'
 const Header = () => {
+  const date = new Date();
+  const greet = date.getHours();
+  const [hour, setHour] = useState(greet)
   return (
     <View style={styles.container}>
       <View>
-        <Text style={styles.text}>Explore</Text>
+        <Text style={styles.text}>{ hour < 12 ? "Good Morning" : "Good Evening"}</Text>
         <Text style={{color : 'white', marginTop : 5}}>Welcome Back John!</Text>
       </View>
       <View >
