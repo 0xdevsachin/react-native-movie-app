bf9aa73bc7ccbaf30d5b7b8d5c95b3a27b062146
sachin320 <sachinvermavs786@gmail.com>
2022-01-18 00:02:14 +0530

    Home Screen Added

---
+++ src/screen/Home/home.js
@@ -0,0 +1,81 @@
+import React, {useEffect, useState} from 'react';
+
+import {View, Text, StyleSheet, ScrollView} from 'react-native';
+import Header from '../../components/header/header';
+import Poster from '../../components/poster/poster'
+import requests from '../../requests'
+import axios from 'axios';
+import Genere from '../../components/Genere/genere';
+
+const HomeScreen = ({ navigation }) => {
+  const genere = ['Action', 'Comedy', 'Documentary', 'Horror', 'Romance', 'Netflix', 'TopRated']
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      await axios
+        .get(`${requests.baseUrl}${requests.fetchTrending}`)
+        .then(res => {
+          setData(res.data.results);
+        })
+        .catch(err => console.log(err));
+    };
+    fetchData();
+  }, []);
+  return (
+    <View style={styles.container}>
+      <ScrollView>
+        <Header />
+        <View>
+          <Text style={styles.text}>Trending Now</Text>
+        </View>
+        <View style={styles.poster}>
+          <ScrollView horizontal={true}>
+            {data.map(item => {
+              return (
+                <Poster
+                  title={item.title}
+                  key={item.id}
+                  link={`${requests.imageUrl}${item.poster_path}`}
+                  navigation={navigation}
+                />
+              );
+            })}
+          </ScrollView>
+        </View>
+        <View style={styles.genere}>
+          <Text style={styles.text}>Best Genre</Text>
+          <ScrollView horizontal={true}>
+            {genere.map(item => (
+              <Genere key={item} navigation={navigation} title={item} />
+            ))}
+          </ScrollView>
+        </View>
+      </ScrollView>
+    </View>
+  );
+};
+
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#1b212f',
+    height: 100,
+    flex: 1,
+  },
+  poster: {
+    height: 400,
+    flexDirection: 'row',
+    overflow: 'scroll',
+  },
+  text: {
+    color: '#fff',
+    paddingLeft: 20,
+    fontSize: 25,
+    marginTop: 15,
+  },
+  genere: {
+    marginBottom: 20,
+  },
+});
