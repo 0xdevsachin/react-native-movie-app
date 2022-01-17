3073bea4b3c0b24999bd1ed56673d873e057fd17
sachin320 <sachinvermavs786@gmail.com>
2022-01-18 00:02:45 +0530

    Movie Genere Screen added

---
+++ src/screen/Detail/detail.js
@@ -0,0 +1,77 @@
+import React, {useEffect, useState} from 'react';
+import {View, Text, StyleSheet, ScrollView, TouchableOpacity, Alert} from 'react-native';
+import requests from '../../requests';
+import axios from 'axios';
+function Detail({route}) {
+  var genereList = {
+    Action: requests.fetchActionMovies,
+    Comedy: requests.fetchComedyMovies,
+    Documentary: requests.fetchDocumentaries,
+    Horror: requests.fetchHorrorMovies,
+    Romance: requests.fetchRomanceMovies,
+    Netflix: requests.fetchNetflixOriginals,
+    TopRated: requests.fetchTopRated,
+  };
+  const [data, setData] = useState([]);
+  const title = route.params.title;
+  const url = `${requests.baseUrl}${genereList[title]}`;
+  useEffect(() => {
+    const fetchData = async () => {
+      await axios
+        .get(`${url}`)
+        .then(res => {
+          setData(res.data.results);
+        })
+        .catch(err => console.log(err));
+    };
+    fetchData();
+  }, []);
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>{title}</Text>
+      <ScrollView>
+        <View>
+          {data.map(item => {
+            return (
+              <TouchableOpacity onPress={() => Alert.alert("Warning!", "Don't Touch it")} key={item.id} style={styles.view}>
+                <Text style={{color : '#fff'}}>
+                  {!item.original_title ? item.name : item.original_title}
+                </Text>
+              </TouchableOpacity>
+            );
+          })}
+        </View>
+      </ScrollView>
+    </View>
+  );
+}
+
+export default Detail;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#1b212f',
+    height: 100,
+    flex: 1,
+    paddingLeft: 20,
+    paddingRight : 20,
+    paddingTop: 10,
+    color: '#fff',
+  },
+  text: {
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginBottom: 22,
+  },
+  view: {
+    marginBottom: 20,
+    height : 80,
+    paddingLeft : 10,
+    paddingTop : 12,
+    backgroundColor : '#306eb7',
+    justifyContent : 'center',
+    alignItems : 'center',
+    borderRadius : 8
+  },
+});
