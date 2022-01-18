a067786b8956f7e76aa107b77eaec0a4d9123170
sachin320 <sachinvermavs786@gmail.com>
2022-01-18 22:23:11 +0530

    Requests

---
+++ src/requests.js
@@ -0,0 +1,17 @@
+const API_KEY = 'YOUR_API_KEY';
+
+const requests = {
+	baseUrl : `https://api.themoviedb.org/3`,
+	imageUrl : `https://image.tmdb.org/t/p/original/`,
+	fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+	fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_network=123`,
+	fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+	fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+	fetchAnime: `/discover/movie?api_key=${API_KEY}&with_genres=16`,
+	fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+	fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+	fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+	fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`
+};
+
+export default requests;
